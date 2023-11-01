1f4f84dd967281ab10d3c2e04c21f61d270b101b
dl2574 <20144415+dl2574@users.noreply.github.com>
2023-11-01 23:00:52 +0900

    Add search functionallity for title and author

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Book
 
@@ -17,3 +18,16 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+    
+    
+class SearchResultsListView(ListView):
+    model = Book
+    template_name = "books/search_results.html"
+    # queryset = Book.objects.filter(title__icontains="beginners")
+    
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+    
